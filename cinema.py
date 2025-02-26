e8a7f40b7533b683a6364d5a49ac110f394f7668
Visharad Mishra <visharadvidyasagar@gmail.com>
2025-02-26 07:50:07 +0530

    created dict

---
+++ cinema.py
@@ -0,0 +1,7 @@
+# Movie data: Movie name as key, a dictionary with 'min_age' and 'available_seats' as values
+movies = {
+    "Movie 1": {"min_age": 18, "available_seats": 10},
+    "Movie 2": {"min_age": 15, "available_seats": 5},
+    "Movie 3": {"min_age": 12, "available_seats": 0},
+    "Movie 4": {"min_age": 10, "available_seats": 20}
+}
